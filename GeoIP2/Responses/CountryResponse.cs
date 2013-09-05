d132e5d0c7d3c019ffe67a59c7e8c6d3d315c98b
Brian Sullivan <bmsullivan@gmail.com>
2013-09-04 19:10:29 -0500

    Refactor OmniResponse properties into appropriate response classes

---
+++ GeoIP2/Responses/CountryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MaxMind.GeoIP2.Model;
+
+namespace MaxMind.GeoIP2.Responses
+{
+    public class CountryResponse
+    {
+        public Continent Continent { get; internal set; }
+        public Country Country { get; internal set; }
+        public Model.MaxMind MaxMind { get; internal set; }
+        public Country RegisteredCountry { get; internal set; }
+        public RepresentedCountry RepresentedCountry { get; internal set; }
+        public Traits Traits { get; internal set; }
+    }
+}
--- GeoIP2/Responses/OmniResponse.cs
+++ GeoIP2/Responses/OmniResponse.cs
@@ -3,17 +3,7 @@ using MaxMind.GeoIP2.Model;
 
 namespace MaxMind.GeoIP2.Responses
 {
-    public class OmniResponse
+    public class OmniResponse : CityResponse
     {
-        public City City { get; set; }
-        public Continent Continent { get; set; }
-        public Country Country { get; set; }
-        public Location Location { get; set; }
-        public Model.MaxMind MaxMind { get; set; }
-        public Postal Postal { get; set; }
-        public Country RegisteredCountry { get; set; }
-        public RepresentedCountry RepresentedCountry { get; set; }
-        public List<Subdivision> Subdivisions { get; set; }
-        public Traits Traits { get; set; }
     }
 }
