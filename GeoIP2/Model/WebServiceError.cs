a4b36f199691ce015c198f0f009cfc350bc2ce0a
Brian Sullivan <bmsullivan@gmail.com>
2013-09-07 22:57:48 -0500

    Added test for web service error

---
+++ GeoIP2/Model/WebServiceError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaxMind.GeoIP2.Model
+{
+    public class WebServiceError
+    {
+        public string Error { get; set; }
+        public string Code { get; set; }
+    }
+}
