d5a981b1d051c5a0288c631c7fa2b7f67586e89e
Brian Sullivan <bmsullivan@gmail.com>
2013-09-14 00:11:57 -0500

    Add auth tests

---
+++ GeoIP2/Exceptions/GeoIP2AuthenticationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaxMind.GeoIP2.Exceptions
+{
+    public class GeoIP2AuthenticationException : GeoIP2Exception
+    {
+        public GeoIP2AuthenticationException(string message) : base(message)
+        {
+            
+        }
+    }
+}
