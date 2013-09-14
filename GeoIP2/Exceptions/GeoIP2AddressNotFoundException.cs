8f74251cbac16257651331d2756d29fe00993df8
Brian Sullivan <bmsullivan@gmail.com>
2013-09-13 23:55:35 -0500

    Add address not found test

---
+++ GeoIP2/Exceptions/GeoIP2AddressNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MaxMind.GeoIP2.Exceptions
+{
+    public class GeoIP2AddressNotFoundException : GeoIP2Exception
+    {
+        public GeoIP2AddressNotFoundException(string message) : base(message)
+        {
+            
+        }
+    }
+}
