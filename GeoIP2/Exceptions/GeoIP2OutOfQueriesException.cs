5b46c671a9fb3e887cf8028dd7daf2c1290982eb
Brian Sullivan <bmsullivan@gmail.com>
2013-09-14 00:16:25 -0500

    Add out of queries test

---
+++ GeoIP2/Exceptions/GeoIP2OutOfQueriesException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaxMind.GeoIP2.Exceptions
+{
+    public class GeoIP2OutOfQueriesException : GeoIP2Exception
+    {
+        public GeoIP2OutOfQueriesException(string message) : base(message)
+        {
+            
+        }
+    }
+}
