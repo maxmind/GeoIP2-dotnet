9f1a19cc9ebc5e2de4413964ffde626227980631
Brian Sullivan <bmsullivan@gmail.com>
2013-09-14 00:21:39 -0500

    Add doc comments for new exception

--- GeoIP2/Exceptions/GeoIP2AuthenticationException.cs
+++ GeoIP2/Exceptions/GeoIP2AuthenticationException.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace MaxMind.GeoIP2.Exceptions
 {
+    /// <summary>
+    /// This exception is thrown when there is an authentication error.
+    /// </summary>
     public class GeoIP2AuthenticationException : GeoIP2Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeoIP2AuthenticationException"/> class.
+        /// </summary>
+        /// <param name="message">A message explaining the cause of the error.</param>
         public GeoIP2AuthenticationException(string message) : base(message)
         {
             
--- GeoIP2/Exceptions/GeoIP2OutOfQueriesException.cs
+++ GeoIP2/Exceptions/GeoIP2OutOfQueriesException.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace MaxMind.GeoIP2.Exceptions
 {
+    /// <summary>
+    /// This exception is thrown when your account does not have any queries remaining for the called service.
+    /// </summary>
     public class GeoIP2OutOfQueriesException : GeoIP2Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeoIP2OutOfQueriesException"/> class.
+        /// </summary>
+        /// <param name="message">A message that describes the error.</param>
         public GeoIP2OutOfQueriesException(string message) : base(message)
         {
             
