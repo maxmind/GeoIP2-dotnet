714ca84318bb6de588cadff4e5ed3f3f95510230
Gregory Oschwald <goschwald@maxmind.com>
2016-01-13 17:11:23 -0500

    Make most warnings errors. Fix exceptions under medium trust.

--- GeoIP2/Model/NamedEntity.cs
+++ GeoIP2/Model/NamedEntity.cs
@@ -64,10 +64,10 @@ namespace MaxMind.GeoIP2.Model
         }
 
         /// <summary>
-        ///     Returns a <see cref="System.String" /> that represents this instance.
+        ///     Returns a <see cref="string" /> that represents this instance.
         /// </summary>
         /// <returns>
-        ///     A <see cref="System.String" /> that represents this instance.
+        ///     A <see cref="string" /> that represents this instance.
         /// </returns>
         public override string ToString()
         {
