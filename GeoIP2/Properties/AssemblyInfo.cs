bbe045d024af5b5b2405b5e6e951af96acdba5ff
Gregory Oschwald <goschwald@maxmind.com>
2016-12-19 14:05:26 -0800

    Add Mono build files

--- GeoIP2/Properties/AssemblyInfo.cs
+++ GeoIP2/Properties/AssemblyInfo.cs
@@ -44,6 +44,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.6.0.0")]
 [assembly: AssemblyFileVersion("2.6.0")]
+[assembly: AssemblyInformationalVersion("2.6.0")]
 [assembly: InternalsVisibleTo("MaxMind.GeoIP2.UnitTests,PublicKey=" +
                               "0024000004800000940000000602000000240000525341310004000001000100e30b6e4a9425b1" +
                               "617ffc8bdf79801e67a371f9f650db860dc0dfff92cb63258765a0955c6fcde1da78dbaf5bf84d" +
