7c0ced48a5daa38bb3011aa52980c9dede442373
Gregory Oschwald <oschwald@gmail.com>
2017-04-07 14:07:46 -0700

    Update CI config

--- dev-bin/build.sh
+++ dev-bin/build.sh
@@ -8,29 +8,33 @@ if [ -n "${DOTNETCORE:-}" ]; then
 
   echo Using .NET CLI
 
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # This is due to: https://github.com/NuGet/Home/issues/2163#issue-135917905
+    echo "current ulimit is: `ulimit -n`..."
+    ulimit -n 1024
+    echo "new limit: `ulimit -n`"
+  fi
+
   dotnet restore
 
   dotnet build -f netstandard1.4 -c "$CONFIGURATION" ./GeoIP2
 
   # Running Benchmark
-  dotnet run -f netcoreapp1.0 -c "$CONFIGURATION"  -p ./GeoIP2.Benchmark
+  dotnet run -f netcoreapp1.0 -c "$CONFIGURATION"  -p ./GeoIP2.Benchmark/GeoIP2.Benchmark.csproj
 
   # Running Unit Tests
-  dotnet test -f netcoreapp1.0 -c "$CONFIGURATION" ./GeoIP2.UnitTests
+  dotnet test -f netcoreapp1.0 -c "$CONFIGURATION" ./GeoIP2.UnitTests/GeoIP2.UnitTests.csproj
 
 else
 
   echo Using Mono
 
-  pushd mono
-
   nuget restore
 
-  xbuild /p:Configuration=$CONFIGURATION
+  xbuild /p:Configuration=$CONFIGURATION mono/GeoIP2.sln
 
-  mono packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --where "cat != BreaksMono" ./bin/$CONFIGURATION/MaxMind.GeoIP2.UnitTests.dll
+  mono ./packages/xunit.runner.console.2.2.0/tools/xunit.console.exe ./mono/bin/$CONFIGURATION/MaxMind.GeoIP2.UnitTests.dll
 
-  popd
 fi
 
 popd
