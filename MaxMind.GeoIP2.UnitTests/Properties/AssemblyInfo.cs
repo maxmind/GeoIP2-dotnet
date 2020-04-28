400894a361bdd5833991b798d02bba436928194e
Gregory Oschwald <oschwald@gmail.com>
2020-04-28 09:34:23 -0700

    Update copyright year

--- MaxMind.GeoIP2.UnitTests/Properties/AssemblyInfo.cs
+++ MaxMind.GeoIP2.UnitTests/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MaxMind.GeoIP2.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © 2013-2019")]
+[assembly: AssemblyCopyright("Copyright © 2013-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- MaxMind.GeoIP2/Properties/AssemblyInfo.cs
+++ MaxMind.GeoIP2/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MaxMind, Inc.")]
 [assembly: AssemblyProduct("MaxMind.GeoIP2")]
-[assembly: AssemblyCopyright("Copyright © 2013-2019")]
+[assembly: AssemblyCopyright("Copyright © 2013-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
