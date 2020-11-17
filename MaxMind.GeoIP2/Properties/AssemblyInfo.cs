95cdec4c3f74f704c9da43594a79a1529bdc2e40
Gregory Oschwald <oschwald@gmail.com>
2020-11-17 09:00:49 -0800

    Update assembly versions

--- MaxMind.GeoIP2/Properties/AssemblyInfo.cs
+++ MaxMind.GeoIP2/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0")]
 [assembly: AssemblyInformationalVersion("4.0.0")]
 [assembly: InternalsVisibleTo("MaxMind.GeoIP2.UnitTests,PublicKey=" +
